666c47d792925e0b6b33b16ac883889767a7322b
Lucas Wung <tkoppopwung130@gmail.com>
2021-07-22 16:37:27 -0700

    solved pause issue

--- CsiLicense.bat
+++ CsiLicense.bat
@@ -1,6 +1,14 @@
 @echo off
-echo Press Enter...
-"\\gs-5\Computers and Structures\Sentinel RMS 9.7 Utilities\NetKey2\lsmon.exe" gs-5 > test.txt 2>&1
+call getCmdPID
+set "current_pid=%errorlevel%"
+start cmd.exe /c "cd /d "G:\license parsing bot""
+start cmd.exe /c ""\\gs-5\Computers and Structures\Sentinel RMS 9.7 Utilities\NetKey2\lsmon.exe" gs-5 > test.txt 2>&1"
+timeout /t 15
+for /f "skip=3 tokens=2 delims= " %%a in ('tasklist /fi "imagename eq cmd.exe"') do (
+    if "%%a" neq "%current_pid%" (
+        TASKKILL /PID %%a /f >nul 2>nul
+    )
+)
 python3 parse.py
 python3 writetohtm.py
 start license.html
