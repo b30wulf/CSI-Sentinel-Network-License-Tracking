5de75cccff593428c07f304e3a61f64e4c7c3094
Lucas Wung <tkoppopwung130@gmail.com>
2021-08-04 13:46:30 -0700

    extended run time for consistent results

--- CsiLicense.bat
+++ CsiLicense.bat
@@ -1,6 +1,6 @@
 @echo off
 start cmd.exe /c ""\\gs-5\Computers and Structures\Sentinel RMS 9.7 Utilities\NetKey2\lsmon.exe" gs-5 > test.txt 2>&1"
-timeout /t 15
+timeout /t 30
 python3 parse.py
 python3 writetohtm.py
 start license.html
