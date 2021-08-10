e6b748bfaf4e61605264587f562e50845ea8a8dd
Lucas Wung <tkoppopwung130@gmail.com>
2021-08-10 09:56:25 -0700

    ensures lsmon.exe finishes its output first

--- CsiLicense.bat
+++ CsiLicense.bat
@@ -1,6 +1,13 @@
 @echo off
 start "lsmonList" cmd /k "\\gs-5\Computers and Structures\Sentinel RMS 9.7 Utilities\NetKey2\lsmon.exe" gs-5 ^> test.txt 2^>^&1
 timeout /t 30
+:loop
+timeout /t 5
+>nul find "Press Enter to continue" test.txt && (
+  echo Task Completed.
+) || (
+  goto loop
+)
 python3 parse.py
 python3 writetohtm.py
 start license.html
