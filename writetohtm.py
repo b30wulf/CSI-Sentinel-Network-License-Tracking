7096baa4bb060f4339509014307da98de151d449
Lucas Wung <tkoppopwung130@gmail.com>
2021-08-12 15:32:26 -0700

    written to a specific server.

--- writetohtm.py
+++ writetohtm.py
@@ -4,7 +4,7 @@ from datetime import date
 today = date.today()
 d2 = today.strftime("%B %d, %Y")
 now = datetime.now()
-current_time = now.strftime("%I:%M:%S:%p")
+current_time = now.strftime("%I:%M:%S %p")
 with open("//gs-3/CSI/license.html", "w") as e:
     e.write("<img src='https://glotmansimpson.com/wp-content/uploads/2020/03/Glotman-Simpson-Logo-1-1024x178.jpg' alt='gsheader' width='512' height='89'>")
     e.write("<pre><b> This is the CSI/StructurePoint License Tracker.</b></pre>")
