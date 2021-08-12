56ebfee24543f60250414a1a64ca97b44be33b42
Lucas Wung <tkoppopwung130@gmail.com>
2021-08-12 14:55:41 -0700

    added endless looping, and 12hour time (not 24)

--- CsiLicense.bat
+++ CsiLicense.bat
@@ -1,4 +1,5 @@
 @echo off
+:flag
 start "lsmonList" cmd /k "\\gs-5\Computers and Structures\Sentinel RMS 9.7 Utilities\NetKey2\lsmon.exe" gs-5 ^> test.txt 2^>^&1
 timeout /t 30
 :loop
@@ -10,5 +11,5 @@ timeout /t 5
 )
 python3 parse.py
 python3 writetohtm.py
-start license.html
 taskkill /FI "WindowTitle eq lsmonList*" /T /F
+goto flag
--- writetohtm.py
+++ writetohtm.py
@@ -4,10 +4,10 @@ from datetime import date
 today = date.today()
 d2 = today.strftime("%B %d, %Y")
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-with open("license.html", "w") as e:
+current_time = now.strftime("%I:%M:%S:%p")
+with open("//gs-3/CSI/license.html", "w") as e:
     e.write("<img src='https://glotmansimpson.com/wp-content/uploads/2020/03/Glotman-Simpson-Logo-1-1024x178.jpg' alt='gsheader' width='512' height='89'>")
-    e.write("<pre><b> This is a CSI License Tracker to show how many Licenses per program are being used and who is using them.</b></pre>")
+    e.write("<pre><b> This is the CSI/StructurePoint License Tracker.</b></pre>")
     e.write("<pre><b> Refreshed : " + str(d2) + " at "+ str(current_time) + "</b></pre>")
     for lines in contents.readlines():
         if "Licenses being used" in lines:
